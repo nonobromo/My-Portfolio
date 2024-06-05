b9ed1aa43fcabf722e34074d7f66c73f5695b21c
nonobromo <noamab22@gmail.com>
2024-06-05 17:41:53 +0300

    responsive changes, and service button styles changed, and shorter text for each section

--- scriptFiles/main.js
+++ scriptFiles/main.js
@@ -10,6 +10,9 @@ const JS_BTN = document.getElementById("js-btn");
 const HTML_ITEMS = document.querySelector(".portfolio-items");
 const JS_ITEMS = document.querySelector(".js-items")
 const serviceDiv = document.querySelector(".service");
+const sectionPara = document.querySelector(".port-para");
+let eventHandle;
+
 HTML_BTN.addEventListener("click", () => {
     HTML_ITEMS.style.display = "grid";
     JS_ITEMS.style.display = "none"
@@ -22,6 +25,9 @@ HTML_BTN.addEventListener("click", () => {
     HTML_ITEMS.classList.remove("fade-in")
     HTML_ITEMS.removeAttribute("style");
     serviceDiv.removeAttribute("style");
+    sectionPara.innerHTML = "Landing Pages Created Using HTML/CSS";
+    JS_BTN.className = "btn btn-outline-warning";
+    HTML_BTN.className = "btn btn-danger"
 })
 
 
@@ -35,4 +41,8 @@ JS_BTN.addEventListener("click", () => {
     }
     setTimeout(createJsCards, 200)
     JS_ITEMS.classList.remove("fade-in")
+    sectionPara.innerHTML = "Games And Tools Created using Javascript and APIs";
+    HTML_BTN.className = "btn btn-outline-danger"
+    JS_BTN.className = "btn btn-warning"
 })
+
